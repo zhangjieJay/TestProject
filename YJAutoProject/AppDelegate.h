a4b7f76b33f327e2ca44a891ca5669c3801c2ac8
张杰 <JayZhang@zhangjiedemacbook-pro.local>
2016-11-29 21:06:47 +0800

    Initial Commit

---
+++ YJAutoProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJAutoProject
+//
+//  Created by 张杰 on 16/11/29.
+//  Copyright © 2016年 JayZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJAutoProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJAutoProject
+//
+//  Created by 张杰 on 16/11/29.
+//  Copyright © 2016年 JayZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
